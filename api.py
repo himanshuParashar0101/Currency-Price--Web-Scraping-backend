65b0e1d23f8b6f5cb4ff6483fe381de60152c696
Himanshu Parashar <himanshuparashar0101@gmail.com>
2024-08-19 19:35:37 +0530

    Update api.py

--- api.py
+++ api.py
@@ -133,4 +133,5 @@ def get_forex_data():
     return jsonify(response)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
